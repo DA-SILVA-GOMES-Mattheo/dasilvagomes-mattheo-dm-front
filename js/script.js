8d75e5029b714db6e76c45785ca3826b68d6f30f
Matthéo <dasilvagomesmattheo@gmail.com>
2022-09-28 15:59:16 +0200

    Commit 2 AppendChild 5 Elements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-const monTableau = document.getElementById("conteneur");
-console.log(monTableau.innerHTML);
+let element1 = document.createElement("h5");
+let myContent = document.createTextNode("Element 4");
+element1.appendChild(myContent);
+element1.classList.add("element4");
+document.getElementById("conteneur").appendChild(element1);
+
+let element2 = document.createElement("h5");
+let myContent2 = document.createTextNode("Element 5");
+element2.appendChild(myContent2);
+element2.classList.add("element5");
+document.getElementById("conteneur").appendChild(element2);
+console.log(document.getElementById("conteneur").innerHTML);
